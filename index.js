e4616f324e8935f122039170b5444233b3f46081
meolalala <57823331+meolalala@users.noreply.github.com>
2020-11-10 23:17:48 -0500

    danbooru support
    also extra insults and compliments

--- index.js
+++ index.js
@@ -2,33 +2,36 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const fetch = require('node-fetch');
+const Danbooru = require('danbooru')
 
-const insults = ['bitch', 'fuckhead', 'stupid idiot', 'why are you even talking', 'i hate u', 'be quiet stinky fox'];
-const compliments = ['hi mushbun!!!', 'hows the bun today!!!', 'wao a qt!!!!', 'big hearts for smol bun', 'wowowowow urcute'];
+const booru = new Danbooru
+
+const insults = ['bitch', 'fuckhead', 'stupid idiot', 'why are you even talking', 'i hate u', 'be quiet stinky fox', 'lol who lets a fox inside', 'what kind of idiot are you', 'if there were two people left on earth and it was me and you I still wouldn\'t be nice', 'when you look at mirrors they crack', 'shut the fuck up', 'go eat some meat stinky'];
+const compliments = ['hi mushbun!!!', 'hows the bun today!!!', 'wao a qt!!!!', 'big hearts for smol bun', 'wowowowow urcute', 'here you can have some tails to rest on', 'make sure you\'re drinking water!!', 'make sure to eat!!', 'don\'t stay up too late!!', 'remember I\'ll always love you~'];
+
+const booruRandomPosts = () => {
+    booru.posts({ random: true }).then(posts => {
+        const index = Math.floor(Math.random() * posts.length)
+        const post = posts[index]
+        client.channels.cache.get('775896670348509215').send(post.file_url)
+    })
+}
 
 client.once('ready', () => {
     client.user.setPresence({
         activity: { name: 'meola be dumb', type: 'WATCHING' }, status: 'idle'
     });
     console.log('Ready!');
+    setInterval(booruRandomPosts, 3600000)
 });
 
-
-// client.on('message', message => {
-//     if (message.author.id === '111577705044066304' && message.channel.type === 'dm') {
-//         message.channel.send(insults[Math.floor(Math.random() * insults.length)]);
-//     } else if (message.author.id === '87116290535198720' && message.channel.type === 'dm') {
-//         message.channel.send(compliments[Math.floor(Math.random() * compliments.length)]);
-//     } else if (message.content === 'hi meobot') {
-//         message.channel.send('hi!!!');
-//     }
-// });
-
 client.on('message', message => {
-    if (message.content === 'gimme porn') {
-        const file = fetch('https://testbooru.donmai.us/posts/random').then(response => response.json())
-
-        message.channel.send(file);
+    if (message.author.id === '111577705044066304' && message.channel.type === 'dm') {
+        message.channel.send(insults[Math.floor(Math.random() * insults.length)]);
+    } else if (message.author.id === '87116290535198720' && message.channel.type === 'dm') {
+        message.channel.send(compliments[Math.floor(Math.random() * compliments.length)]);
+    } else if (message.content === 'hi meobot') {
+        message.channel.send('hi!!!');
     }
 });
 
